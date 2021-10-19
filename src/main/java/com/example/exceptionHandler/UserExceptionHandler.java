38eb7f5d44330f750be8aa35fb14686214be5e94
liyuan <1987151116@qq.com>
2021-10-19 18:14:30 +0800

    add codeInvalidException and controlleradvice

--- src/main/java/com/example/exceptionHandler/UserExceptionHandler.java
+++ src/main/java/com/example/exceptionHandler/UserExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.exceptionHandler;
 
 import com.example.constants.UserStatusCode;
+import com.example.exception.DynamicCodeExpiredException;
+import com.example.exception.DynamicCodeInvalidException;
 import com.example.exception.UsernameAlreadyExistException;
 import com.example.utils.ResponseUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,5 +14,15 @@ public class UserExceptionHandler {
     @ExceptionHandler({UsernameAlreadyExistException.class})
     public String usernameAlreadyExistException(UsernameAlreadyExistException e) {
         return ResponseUtils.build(UserStatusCode.USERNAME_ALREADY_EXIST, e.getMessage());
+    }
+
+    @ExceptionHandler({DynamicCodeInvalidException.class})
+    public String dynamicCodeInvalidException(DynamicCodeInvalidException e) {
+        return ResponseUtils.build(UserStatusCode.DYNAMIC_CODE_INVALID, e.getMessage());
+    }
+
+    @ExceptionHandler({DynamicCodeExpiredException.class})
+    public String dynamicCodeExpiredException(DynamicCodeExpiredException e) {
+        return ResponseUtils.build(UserStatusCode.DYNAMIC_CODE_EXPIRED, e.getMessage());
     }
 }
